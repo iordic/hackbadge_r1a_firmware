891d3af248ee26fceccf933f85dc26ff5c0e5333
Jordi Castelló <iordicdev@gmail.com>
2025-10-31 00:29:13 +0100

    Add: testing code

---
+++ src/config.h
@@ -0,0 +1,34 @@
+#define RECEIVE_ATTR IRAM_ATTR
+
+#define SERIAL_BAUDRATE 115200
+
+#define OLED_RESET     -1
+#define SCREEN_ADDRESS 0x3c
+#define SCREEN_WIDTH   128
+#define SCREEN_HEIGHT  64
+#define NUM_LEDS 2
+
+/* I/O */
+#ifdef ESP32_C3_SUPERMINI
+    /* Strapping GPIOs:
+     *   2 -> Must be HIGH during reset, if this pin goes low during reset, either flash execution or UART flashing may not work.
+     *   8 -> Must be HIGH during reset, UART flashing may not work if this pin goes low during reset.
+     *   0, 9 -> Works as normal GPIOs after reset (Where it must be high for regular execution and low to flash an application).
+     */
+    //#define BUILTIN_LED 8
+    #define CC1101_SCLK  4
+    #define CC1101_MISO  5
+    #define CC1101_MOSI  6
+    #define CC1101_CS    7 
+    #define CC1101_GDO0 20
+    #define CC1101_GDO2 -1 //21
+    #define I2C_SDA 10
+    #define I2C_SCL  9
+    #define NEOPIXEL 0
+    // buttons
+    #define BUTTON_LEFT  21
+    #define BUTTON_UP     2
+    #define BUTTON_RIGHT  3
+    #define BUTTON_DOWN   1
+    #define BUTTON_ENTER  8
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,75 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <OneButton.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_NeoPixel.h>
+#include <ELECHOUSE_CC1101.h>
+
+#include "config.h"
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_NeoPixel strip(NUM_LEDS, NEOPIXEL, NEO_GRB + NEO_KHZ800);
+OneButton leftBtn, upBtn, rightBtn, downBtn, enterBtn;
+ELECHOUSE_CC1101 *cc1101;
+
+static void handleLeftClick() {
+  Serial.println("Clicked left!");
+}
+static void handleUpClick() {
+  Serial.println("Clicked up!");
+}
+static void handleRightClick() {
+  Serial.println("Clicked right!");
+}
+static void handleDownClick() {
+  Serial.println("Clicked down!");
+}
+static void handleEnterClick() {
+  Serial.println("Clicked enter!");
+}
+
+
+void setup() {
+  Wire.begin(I2C_SDA, I2C_SCL);
+  Serial.begin(SERIAL_BAUDRATE);
+  cc1101 = new ELECHOUSE_CC1101(CC1101_GDO0, CC1101_GDO2, CC1101_SCLK, CC1101_MISO, CC1101_MOSI, CC1101_CS, FSPI);
+  cc1101->init();
+  // Buttons setup
+  leftBtn.setup(BUTTON_LEFT, INPUT_PULLUP, true);
+  upBtn.setup(BUTTON_UP, INPUT_PULLUP, true);
+  rightBtn.setup(BUTTON_RIGHT, INPUT_PULLUP, true);
+  downBtn.setup(BUTTON_DOWN, INPUT_PULLUP, true);
+  enterBtn.setup(BUTTON_ENTER, INPUT_PULLUP, true);
+
+  leftBtn.attachClick(handleLeftClick);
+  rightBtn.attachClick(handleRightClick);
+  upBtn.attachClick(handleUpClick);
+  downBtn.attachClick(handleDownClick);
+  enterBtn.attachClick(handleEnterClick);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+  
+  for(int i=0;i<NUM_LEDS;i++) {
+    strip.setPixelColor(i, strip.Color(random(256), random(256), random(256)));
+  }
+  strip.show();
+  if (cc1101->getCC1101()) {
+    Serial.println("CC1101 conected");
+  } else {
+    Serial.println("CC1101 not conected");
+  }
+}
+
+void loop() {
+  leftBtn.tick();
+  upBtn.tick();
+  rightBtn.tick();
+  downBtn.tick();
+  enterBtn.tick();
+}
