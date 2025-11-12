71584f548c69080649855b5ec5e0a61be784dddf
Jordi <iordicdev@gmail.com>
2025-11-12 10:36:57 +0100

    Add: original hackbadge r1a pins to config

--- src/config/io_config.h
+++ src/config/io_config.h
@@ -11,7 +11,7 @@
 #define NUM_LEDS 2
 
 /* I/O */
-#ifdef ESP32_C3_SUPERMINI
+#ifdef ESP32_C3_SUPERMINI_CUSTOM
     /* Strapping GPIOs:
      *   2 -> Must be HIGH during reset, if this pin goes low during reset, either flash execution or UART flashing may not work.
      *   8 -> Must be HIGH during reset, UART flashing may not work if this pin goes low during reset.
@@ -33,5 +33,22 @@
     #define BUTTON_RIGHT  3
     #define BUTTON_DOWN   1
     #define BUTTON_ENTER  8
+#endif
+#ifdef HACKBADGE_R1A
+    #define CC1101_SCLK 10
+    #define CC1101_MISO  3
+    #define CC1101_MOSI  21 //4
+    #define CC1101_CS   20 
+    #define CC1101_GDO0 4 //21 // -> pin 21 cannot be used as INPUT, so we remap GDO0 to pin 4
+    #define CC1101_GDO2 -1 //21 // we are out of pins, so we must use gdo0 for both, receive and transmit
+    #define I2C_SDA      1
+    #define I2C_SCL      0
+    #define NEOPIXEL     2
+    // buttons
+    #define BUTTON_LEFT  9
+    #define BUTTON_UP    8
+    #define BUTTON_RIGHT 7
+    #define BUTTON_DOWN  6
+    #define BUTTON_ENTER 5
 #endif
 #endif
